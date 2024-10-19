257177f5096e1f6717078c0f01f328094dd9ae35
Andre <47895843+andreburg@users.noreply.github.com>
2024-10-19 21:14:57 +0200

    chore(server): Add routes folder

--- web/server/src/index.ts
+++ web/server/src/index.ts
@@ -1,14 +1,17 @@
-import express, { Request, Response, Application } from "express";
+import express, { Application } from "express";
 import dotenv from "dotenv";
 import * as env from "./utils/env";
+import { indexRouter } from "./routes";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Application = express();
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("APEX CARE SOLUTIONS API");
-});
+app.use(cors());
+app.use(express.json());
+
+app.use("/", indexRouter);
 
 app.listen(env.PORT, () => {
     console.log(`Server listeinng at http://localhost:${env.PORT}`);
--- web/server/src/utils/env.ts
+++ web/server/src/utils/env.ts
@@ -1,12 +1,17 @@
-function requireEnv(varName: string, defaultValue: string | number) {
+function requireEnv(varName: string, defaultValue?: string | number) {
     let envVar = process.env[varName];
     if (!envVar) {
         console.warn(
             `Environment variable \`${varName}\` is not set.\nReverting to default.`,
         );
+        if (!defaultValue) {
+            throw new Error(
+                `ERROR: Environment variable \`${varName}\` default not set`,
+            );
+        }
         return defaultValue;
     }
     return envVar;
 }
 
-export const PORT = requireEnv("PORT", 3000);
+export const PORT = requireEnv("PORT", 3333);
