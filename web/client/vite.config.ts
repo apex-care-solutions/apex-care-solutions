2ad94d2fd6c92fb086bc26ebb3054c27b24bbacc
Andre <47895843+andreburg@users.noreply.github.com>
2024-10-20 14:37:04 +0200

    build: Update vite config
    Added polling for hot reloading.

--- web/client/vite.config.ts
+++ web/client/vite.config.ts
@@ -14,5 +14,8 @@ export default defineConfig({
         strictPort: true,
         host: true,
         origin: "http://0.0.0.0:3003",
+        watch: {
+            usePolling: true,
+        },
     },
 });
