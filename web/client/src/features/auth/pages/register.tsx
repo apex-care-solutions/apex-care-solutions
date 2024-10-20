ae371701d416bcc49241b2e0b2938a71d5846143
Werner Janse van Rensburg <108306252+Werriess@users.noreply.github.com>
2024-10-20 16:09:44 +0200

    feat(register/login): Add page design code (#31)
    From design create register and login page
    
    refs: #30

---
+++ web/client/src/features/auth/pages/register.tsx
@@ -0,0 +1,47 @@
+import { AuthContainer } from "../components/auth-container";
+import { Button } from "@/components/ui/button";
+import "/src/globals.css";
+
+export function Register() {
+    function RegisterForm() {
+        return (
+            <form className="flex flex-col gap-4 w-96">
+                <input
+                    type="text"
+                    placeholder="Enter first name"
+                    className="bg-white-muted border p-2"
+                />
+                <input
+                    type="text"
+                    placeholder="Enter last name"
+                    className="bg-white-muted border p-2"
+                />
+                <input
+                    type="email"
+                    placeholder="Enter last name"
+                    className="bg-white-muted border p-2"
+                />
+                <input
+                    type="password"
+                    placeholder="Enter password"
+                    className="bg-white-muted border p-2"
+                />
+                <input
+                    type="password"
+                    placeholder="Confirm password"
+                    className="bg-white-muted border p-2"
+                />
+                <Button variant="default" size="default" className="mt-10">
+                    Register
+                </Button>
+            </form>
+        );
+    }
+    return (
+        <div>
+            <AuthContainer>
+                <RegisterForm/>
+            </AuthContainer>
+        </div>
+    );
+}
