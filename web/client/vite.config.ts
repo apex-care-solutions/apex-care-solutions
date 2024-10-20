3e9d562a13d501ad76e37a989c74c663ca41ea7c
Andre <47895843+andreburg@users.noreply.github.com>
2024-10-20 12:14:22 +0200

    build: Add client to docker compose

--- web/client/vite.config.ts
+++ web/client/vite.config.ts
@@ -3,10 +3,16 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
+    server: {
+        port: 3003,
+        strictPort: true,
+        host: true,
+        origin: "http://0.0.0.0:8080",
+    },
 });
