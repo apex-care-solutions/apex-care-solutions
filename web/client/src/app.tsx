fc2163bbb2802836af92f88e59f79aa6188bfbf7
Andre <47895843+andreburg@users.noreply.github.com>
2024-10-24 20:56:18 +0200

    fix(client): Move router to encapsulate nav

--- web/client/src/app.tsx
+++ web/client/src/app.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router } from "react-router-dom";
 import { LandingNav } from "./presenter/components/layout/nav";
 import { CoreNavigation } from "./presenter/features/core";
 import "@/globals.css";
@@ -5,8 +6,10 @@ import "@/globals.css";
 export function App() {
     return (
         <main>
-            <LandingNav />
-            <CoreNavigation />
+            <Router>
+                <LandingNav />
+                <CoreNavigation />
+            </Router>
         </main>
     );
 }
--- web/client/src/presenter/features/core/components/navigation/core-navigation.tsx
+++ web/client/src/presenter/features/core/components/navigation/core-navigation.tsx
@@ -1,15 +1,13 @@
 import { Login, Register } from "@/presenter/features/auth";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "@/presenter/features/core";
 
 export function CoreNavigation() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+        </Routes>
     );
 }
