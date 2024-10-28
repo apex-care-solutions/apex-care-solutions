f8baa7a7601f95a811fa2bb9178a7e90f97e53f9
Werner Janse van Rensburg <108306252+Werriess@users.noreply.github.com>
2024-10-28 22:23:58 +0200

    feat(home): Create home page (#40)
    From design make home page

--- web/client/src/presenter/features/core/views/home.tsx
+++ web/client/src/presenter/features/core/views/home.tsx
@@ -1,42 +1,86 @@
-import { Button } from "@/presenter/components/ui/button";
-import "/src/globals.css";
-import { ArrowRight } from "lucide-react";
-import { useSession } from "../../auth/context/auth-provider";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export function Home() {
-    const [user] = useSession();
     return (
-        <div className="flex flex-col gap-20">
-            <div className="flex flex-row max-md:flex-col gap-20">
-                <div className="flex-1 flex flex-col justify-center gap-5">
-                    <p className="text-6xl font-bold">
-                        Your Equiptment, Our Care.
-                    </p>
-                    <p className="text-4xl text-muted-foreground">
-                        Anytime, Every Time
-                    </p>
-                    <p className="font-bold text-muted-foreground">
-                        Tell us what you need, and we'll be there, guaranteed!
-                    </p>
-                    <div className="flex gap-5">
-                        <input
-                            type="text"
-                            className="flex-1 rounded-full px-5 bg-muted text-muted-foreground"
-                        />
-                        <Button
-                            size="icon"
-                            className="rounded-full bg-muted text-muted-foreground hover:text-primary-foreground hover:bg-muted-foreground"
-                        >
-                            <ArrowRight />
-                        </Button>
+        <div>
+            <div className="bg-muted">
+                <div className="flex items-center justify-between m-5">
+                    <div className="bg-black text-white p-4">#22</div>
+                    <div className="flex justify-center bg-amber-500 text-white p-2 w-3/5 rounded">
+                        In progress
+                    </div>
+                    <div>
+                        <img src="/expand.svg" alt="expand" />
                     </div>
                 </div>
-                <div className="flex-1 rounded-lg overflow-hidden">
-                    <img
-                        src="/assets/hero-img.png"
-                        alt="hero"
-                        className="w-full"
-                    />
+            </div>
+            <div className="bg-black flex items-center justify-center p-10">
+                <img src="/promoNew.svg" alt="promo" />
+            </div>
+            <div className="flex">
+                <div className="basis-4/5 p-4 bg-stone-100">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Iusto corrupti temporibus quidem sapiente,
+                        repellat excepturi odit illum, libero deserunt, commodi
+                        quia ullam praesentium reiciendis? Quos officiis
+                        explicabo soluta in aut!
+                    </p>
+                </div>
+                <div className="basis-1/5 p-4 bg-stone-300">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Ipsam error, molestiae iure perferendis quas, provident
+                        quos quis enim aut aliquam ullam adipisci expedita
+                        maxime vero libero similique autem nobis vel!
+                    </p>
+                </div>
+            </div>
+            <div>
+                <div className="flex flex-col justify-center items-center mt-10 gap-2">
+                    <p className="text-4xl font-bold">
+                        BE PREPARED FOR EVERYTHING
+                    </p>
+                    <p>
+                        Choose a plan to fit your needs from our expansive
+                        selction of services and packages
+                    </p>
+                </div>
+                <div className="flex justify-center mt-5">
+                    <Carousel
+                        opts={{
+                            align: "start",
+                        }}
+                        className="w-full max-w-sm"
+                    >
+                        <CarouselContent>
+                            {Array.from({ length: 5 }).map((_, index) => (
+                                <CarouselItem
+                                    key={index}
+                                    className="md:basis-1/2 lg:basis-1/3"
+                                >
+                                    <div className="p-1">
+                                        <Card>
+                                            <CardContent className="flex aspect-square items-center justify-center p-6">
+                                                <span className="text-3xl font-semibold">
+                                                    {index + 1}
+                                                </span>
+                                            </CardContent>
+                                        </Card>
+                                    </div>
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                        <CarouselPrevious />
+                        <CarouselNext />
+                    </Carousel>
                 </div>
             </div>
         </div>
