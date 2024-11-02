1ccf2806484014f90b95c383b5c393d0c631c802
Andre <47895843+andreburg@users.noreply.github.com>
2024-11-02 21:35:58 +0200

    refactor: Add a touchup to home page features
    Added a touchup to homepage features making them more functional and resemble the final state more closely.

--- web/client/src/presenter/features/core/views/home.tsx
+++ web/client/src/presenter/features/core/views/home.tsx
@@ -7,101 +7,61 @@ import {
     CarouselPrevious,
 } from "@/components/ui/carousel";
 import { JobPreview } from "../../job/components/job-preview";
+import { FAQList } from "../components/faq-list";
 
 export function Home() {
     return (
         <div className="flex flex-col gap-10">
             <JobPreview />
-            <div className="bg-black flex items-center justify-center p-10">
-                <img src="/promoNew.svg" alt="promo" />
-            </div>
-            <div className="flex">
-                <div className="basis-4/5 p-4 bg-stone-100">
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Iusto corrupti temporibus quidem sapiente,
-                        repellat excepturi odit illum, libero deserunt, commodi
-                        quia ullam praesentium reiciendis? Quos officiis
-                        explicabo soluta in aut!
-                    </p>
+            <div className="flex flex-col">
+                <div className="bg-black flex items-center justify-center p-10">
+                    <img src="/promoNew.svg" alt="promo" />
                 </div>
-                <div className="basis-1/5 p-4 bg-stone-300">
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Ipsam error, molestiae iure perferendis quas, provident
-                        quos quis enim aut aliquam ullam adipisci expedita
-                        maxime vero libero similique autem nobis vel!
-                    </p>
+                <div className="flex min-h-52 h-52">
+                    <div className="flex-1 h-full p-4 bg-stone-200"></div>
+                    <div className="flex-1 h-full p-4 bg-stone-100"></div>
+                    <div className="flex-1 h-full p-4 bg-stone-300"></div>
                 </div>
             </div>
-            <div>
-                <div className="flex flex-col justify-center items-center mt-10 gap-2">
-                    <p className="text-4xl font-bold">
-                        BE PREPARED FOR EVERYTHING
-                    </p>
-                    <p>
-                        Choose a plan to fit your needs from our expansive
-                        selction of services and packages
-                    </p>
-                </div>
-                <div className="flex justify-center mt-5">
-                    <Carousel
-                        opts={{
-                            align: "start",
-                        }}
-                        className="w-full max-w-sm"
-                    >
-                        <CarouselContent>
-                            {Array.from({ length: 10 }).map((_, index) => (
-                                <CarouselItem
-                                    key={index}
-                                    className="md:basis-1/3 lg:basis-1/5"
-                                >
-                                    <div className="p-1">
-                                        <Card>
-                                            <CardContent className="flex aspect-square items-center justify-center p-6">
-                                                <span className="text-3xl font-semibold">
-                                                    {index + 1}
-                                                </span>
-                                            </CardContent>
-                                        </Card>
-                                    </div>
-                                </CarouselItem>
-                            ))}
-                        </CarouselContent>
-                        <CarouselPrevious />
-                        <CarouselNext />
-                    </Carousel>
-                </div>
-                <div className="flex flex-col gap-5">
-                    <p className="text-3xl font-bold max-w-96">
-                        Frequently Asked questions
-                        <span className="text-amber-500">.</span>
-                    </p>
-                    <div className="flex justify-between items-center border-b border-black p-2 font-bold">
-                        <p>What services does ApexCare Solutions provide?</p>
-                        <div>+</div>
-                    </div>
-                    <div className="flex justify-between items-center border-b border-black p-2 font-bold">
-                        <p>How do request a service from ApexCare Solutions?</p>
-                        <div>+</div>
-                    </div>
-                    <div className="flex justify-between items-center border-b border-black p-2 font-bold">
-                        <p>Can I track the progress of my service request?</p>
-                        <div>+</div>
-                    </div>
-                    <div className="flex justify-between items-center border-b border-black p-2 font-bold">
-                        <p>
-                            How do I provide feedback on the service provided?
-                        </p>
-                        <div>+</div>
-                    </div>
-                    <div className="flex justify-between items-center border-b border-black p-2 font-bold">
-                        <p>Can I setup a recurring subscription?</p>
-                        <div>+</div>
-                    </div>
-                </div>
+            <div className="flex flex-col justify-center items-center mt-10 gap-5">
+                <p className="text-5xl font-bold text-primary/90">
+                    BE PREPARED FOR EVERYTHING
+                </p>
+                <p className="font-bold text-xl text-muted-foreground text-center">
+                    Choose a plan to fit your needs from our expansive <br />
+                    selction of services and packages
+                </p>
             </div>
+            <div className="flex justify-center mt-5">
+                <Carousel
+                    opts={{
+                        align: "start",
+                    }}
+                    className="w-full"
+                >
+                    <CarouselContent>
+                        {Array.from({ length: 10 }).map((_, index) => (
+                            <CarouselItem
+                                key={index}
+                                className="md:basis-1/3 lg:basis-1/5"
+                            >
+                                <div className="p-1">
+                                    <Card>
+                                        <CardContent className="flex aspect-square items-center justify-center p-6">
+                                            <span className="text-3xl font-semibold">
+                                                {index + 1}
+                                            </span>
+                                        </CardContent>
+                                    </Card>
+                                </div>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
+            </div>
+            <FAQList />
         </div>
     );
 }
--- web/client/tailwind.config.ts
+++ web/client/tailwind.config.ts
@@ -11,101 +11,105 @@ const config = {
     ],
     prefix: "",
     theme: {
-        container: {
-            center: true,
-            padding: "2rem",
-            screens: {
-                "2xl": "1400px",
-            },
-        },
-        extend: {
-            fontSize: {
-                xxs: "0.625rem",
-                xxxs: "0.5rem",
-            },
-            colors: {
-                border: "hsl(var(--border))",
-                input: "hsl(var(--input))",
-                ring: "hsl(var(--ring))",
-                background: "hsl(var(--background))",
-                foreground: "hsl(var(--foreground))",
-                primary: {
-                    DEFAULT: "hsl(var(--primary))",
-                    foreground: "hsl(var(--primary-foreground))",
-                    muted: {
-                        DEFAULT: "hsl(var(--primary) / 0.5)",
-                        foreground: "hsl(var(--primary-foreground) / 0.5)",
-                    },
-                },
-                secondary: {
-                    DEFAULT: "hsl(var(--secondary))",
-                    foreground: "hsl(var(--secondary-foreground))",
-                },
-                destructive: {
-                    DEFAULT: "hsl(var(--destructive))",
-                    foreground: "hsl(var(--destructive-foreground))",
-                },
-                success: {
-                    DEFAULT: "hsl(var(--success))",
-                    foreground: "hsl(var(--success-foreground))",
-                },
-                muted: {
-                    DEFAULT: "hsl(var(--muted))",
-                    foreground: "hsl(var(--muted-foreground))",
-                },
-                popover: {
-                    DEFAULT: "hsl(var(--popover))",
-                    foreground: "hsl(var(--popover-foreground))",
-                },
-                card: {
-                    DEFAULT: "hsl(var(--card))",
-                    foreground: "hsl(var(--card-foreground))",
-                },
-                accent: {
-                    DEFAULT: "hsl(var(--accent))",
-                    foreground: "hsl(var(--accent-foreground))",
-                },
-                chart: {
-                    "1": "hsl(var(--chart-1))",
-                    "2": "hsl(var(--chart-2))",
-                    "3": "hsl(var(--chart-3))",
-                    "4": "hsl(var(--chart-4))",
-                    "5": "hsl(var(--chart-5))",
-                },
-            },
-            borderRadius: {
-                lg: "var(--radius)",
-                md: "calc(var(--radius) - 2px)",
-                sm: "calc(var(--radius) - 4px)",
-            },
-            keyframes: {
-                "accordion-down": {
-                    from: {
-                        height: "0",
-                    },
-                    to: {
-                        height: "var(--radix-accordion-content-height)",
-                    },
-                },
-                "accordion-up": {
-                    from: {
-                        height: "var(--radix-accordion-content-height)",
-                    },
-                    to: {
-                        height: "0",
-                    },
-                },
-                'pulse-accent': {
-                    '0%, 25%, 75%, 100%': { backgroundColor: 'hsl(var(--primary-foreground))'  },
-                    '50%': { backgroundColor: 'hsl(var(--accent))' },
-                  },
-            },
-            animation: {
-                "accordion-down": "accordion-down 0.2s ease-out",
-                "accordion-up": "accordion-up 0.2s ease-out",
-                'pulse-accent': 'pulse-accent 2s ease-in-out infinite',
-            },
-        },
+    	container: {
+    		center: true,
+    		padding: '2rem',
+    		screens: {
+    			'2xl': '1400px'
+    		}
+    	},
+    	extend: {
+    		fontSize: {
+    			xxs: '0.625rem',
+    			xxxs: '0.5rem'
+    		},
+    		colors: {
+    			border: 'hsl(var(--border))',
+    			input: 'hsl(var(--input))',
+    			ring: 'hsl(var(--ring))',
+    			background: 'hsl(var(--background))',
+    			foreground: 'hsl(var(--foreground))',
+    			primary: {
+    				DEFAULT: 'hsl(var(--primary))',
+    				foreground: 'hsl(var(--primary-foreground))',
+    				muted: {
+    					DEFAULT: 'hsl(var(--primary) / 0.5)',
+    					foreground: 'hsl(var(--primary-foreground) / 0.5)'
+    				}
+    			},
+    			secondary: {
+    				DEFAULT: 'hsl(var(--secondary))',
+    				foreground: 'hsl(var(--secondary-foreground))'
+    			},
+    			destructive: {
+    				DEFAULT: 'hsl(var(--destructive))',
+    				foreground: 'hsl(var(--destructive-foreground))'
+    			},
+    			success: {
+    				DEFAULT: 'hsl(var(--success))',
+    				foreground: 'hsl(var(--success-foreground))'
+    			},
+    			muted: {
+    				DEFAULT: 'hsl(var(--muted))',
+    				foreground: 'hsl(var(--muted-foreground))'
+    			},
+    			popover: {
+    				DEFAULT: 'hsl(var(--popover))',
+    				foreground: 'hsl(var(--popover-foreground))'
+    			},
+    			card: {
+    				DEFAULT: 'hsl(var(--card))',
+    				foreground: 'hsl(var(--card-foreground))'
+    			},
+    			accent: {
+    				DEFAULT: 'hsl(var(--accent))',
+    				foreground: 'hsl(var(--accent-foreground))'
+    			},
+    			chart: {
+    				'1': 'hsl(var(--chart-1))',
+    				'2': 'hsl(var(--chart-2))',
+    				'3': 'hsl(var(--chart-3))',
+    				'4': 'hsl(var(--chart-4))',
+    				'5': 'hsl(var(--chart-5))'
+    			}
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		},
+    		keyframes: {
+    			'accordion-down': {
+    				from: {
+    					height: '0'
+    				},
+    				to: {
+    					height: 'var(--radix-accordion-content-height)'
+    				}
+    			},
+    			'accordion-up': {
+    				from: {
+    					height: 'var(--radix-accordion-content-height)'
+    				},
+    				to: {
+    					height: '0'
+    				}
+    			},
+    			'pulse-accent': {
+    				'0%, 25%, 75%, 100%': {
+    					backgroundColor: 'hsl(var(--primary-foreground))'
+    				},
+    				'50%': {
+    					backgroundColor: 'hsl(var(--accent))'
+    				}
+    			},
+    		},
+    		animation: {
+    			'accordion-down': 'accordion-down 0.2s ease-out',
+    			'accordion-up': 'accordion-up 0.2s ease-out',
+    			'pulse-accent': 'pulse-accent 2s ease-in-out infinite',
+    		}
+    	}
     },
     plugins: [tailwindcssAnimate],
 } satisfies Config;
