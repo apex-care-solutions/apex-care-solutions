4c9671591b5b2d16ebed50148207c5f9aadc5223
andreburg <andreludovicusburger@gmail.com>
2024-11-03 21:16:24 +0200

    feat(auth): Add cookie authentication
    Add cookie authentication to server allowing to identify users when making requests.
    
    refs: #51

--- web/server/src/controllers/auth-controller.ts
+++ web/server/src/controllers/auth-controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import { prisma } from "../repositories/prisma";
 import bcrypt from "bcrypt";
-import { SALT_ROUNDS } from "../utils/env";
+import { JWT_SECRET, SALT_ROUNDS } from "../utils/env";
+import jwt from "jsonwebtoken";
+import { ApexAPIResponse, createApexResponse } from "../utils/api";
 
 
 export const registerUser = async (
@@ -10,12 +12,8 @@ export const registerUser = async (
 ): Promise<any> => {
     try {
         const { firstName, lastName, username, email, phone, password, confirmPassword } = request.body;
-
         if (password !== confirmPassword) return response.status(400).json({ error: "Passwords do not match" });
-        
-
         const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
-
         const res = await prisma.user.create({
             data: {
                 email,
@@ -27,37 +25,40 @@ export const registerUser = async (
                 userType: "CUSTOMER",
             }
         });
-
-        console.log(res)
-
-        response.json({ message: "User registered successfully", user: res });
+        response.json(createApexResponse("OK", undefined, undefined, "/auth/login"));
     } catch (error) {
         console.error("Registration error:", error);
-        response.status(400).json({ error: "Registration failed" });
+        response.status(400).json(createApexResponse("INTERNAL_SERVER_ERROR", undefined, error as string));
     }
 };
 
 export const loginUser = async (request: Request, response: Response): Promise<any> => {
     try {
         const { username, password } = request.body;
-
         const user = await prisma.user.findFirst({
             where: { username },
         });
-
         if (!user) return response.status(401).json({ error: "Invalid credentials" });
-        
-
         const isPasswordValid = await bcrypt.compare(password, user.password);
 
         if (!isPasswordValid) {
-            response.status(401).json({ error: "Invalid credentials" });
+            return response.status(401).json({ error: "Invalid credentials" });
         }
+        const { password: _, ...secureUser } = user;
+        const token = jwt.sign(
+            secureUser,
+            JWT_SECRET,
+        );
+        response.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+        });
 
-        response.json({ message: "Login successful" });
+        response.json(createApexResponse("OK", secureUser, undefined, "/"));
     } catch (error) {
         console.error("Login error:", error);
-        response.status(401).json({ error: "Invalid credentials" });
+        response.json(createApexResponse("INTERNAL_SERVER_ERROR", undefined, error as string));
     }
 };
 
@@ -66,16 +67,30 @@ export const logoutUser = async (
     response: Response,
 ): Promise<void> => {
     try {
-        response.json({ message: "Logout successful" });
+        response.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+        });
+
+        response.json(createApexResponse("OK",undefined, undefined, "/"));
     } catch (error) {
-        response.json({ error: "Logout failed" });
+        console.error("Logout error:", error);
+        response.json(createApexResponse("INTERNAL_SERVER_ERROR"));
     }
 };
 
-export const getUser = async(request: Request, response: Response) : Promise<void> => {
+export const getUser = async (request: Request, response: Response): Promise<void> => {
     try {
-        response.json({ message: "Logout successful" });
+        const token = request.cookies.token;
+        if (!token) {
+            response.json(createApexResponse("INTERNAL_SERVER_ERROR"));
+            return;
+        };
+        const decoded = jwt.verify(token, JWT_SECRET);
+        response.json(createApexResponse("OK",decoded));
     } catch (error) {
-        response.json({ error: "Logout failed" });
+        console.error("Error retrieving user:", error);
+        response.status(500).json({ error: "Failed to retrieve user" });
     }
-}
+};
