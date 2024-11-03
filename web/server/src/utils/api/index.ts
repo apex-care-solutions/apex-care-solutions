5b317420f47da5a450bdd7aeff5ebda1fb9ad274
andreburg <andreludovicusburger@gmail.com>
2024-11-03 21:12:55 +0200

    feat: Add utility for standard responses
    Add Apex Api Response utility function to effectively reference responses attaching and building what the client will recieve
    
    refs: #50

--- web/server/src/utils/api/index.ts
+++ web/server/src/utils/api/index.ts
@@ -1,18 +1,20 @@
-export type ApexAPIResponse<T> = {
+export type ApexAPIResponse = {
     success: boolean,
     statusCode: number,
     message?: string,
-    data?: T,
+    data?: any,
     error?: string,
     redirect?: string
 }
 
+export const createApexResponse = (status: keyof typeof HTTP_STATUS_CODES, data?: any, error?: string, redirect?: string) => ({...HTTP_STATUS_CODES[status], data, error, redirect})
+
 
 export const HTTP_STATUS_CODES = {
-    OK: { statusCode: 200, message: "OK" },
-    CREATED: { statusCode: 201, message: "Created" },
-    ACCEPTED: { statusCode: 202, message: "Accepted" },
-    NO_CONTENT: { statusCode: 204, message: "No Content" },
+    OK: { statusCode: 200, message: "OK", success: true },
+    CREATED: { statusCode: 201, message: "Created", success: true  },
+    ACCEPTED: { statusCode: 202, message: "Accepted",success: true  },
+    NO_CONTENT: { statusCode: 204, message: "No Content",success: true  },
 
     // Redirect status codes
     MOVED_PERMANENTLY: { statusCode: 301, message: "Moved Permanently" },
@@ -22,17 +24,17 @@ export const HTTP_STATUS_CODES = {
     PERMANENT_REDIRECT: { statusCode: 308, message: "Permanent Redirect" },
 
     // Client error status codes
-    BAD_REQUEST: { statusCode: 400, message: "Bad Request" },
-    UNAUTHORIZED: { statusCode: 401, message: "Unauthorized" },
-    FORBIDDEN: { statusCode: 403, message: "Forbidden" },
-    NOT_FOUND: { statusCode: 404, message: "Not Found" },
-    CONFLICT: { statusCode: 409, message: "Conflict" },
-    UNPROCESSABLE_ENTITY: { statusCode: 422, message: "Unprocessable Entity" },
+    BAD_REQUEST: { statusCode: 400, message: "Bad Request",success: false  },
+    UNAUTHORIZED: { statusCode: 401, message: "Unauthorized",success: false },
+    FORBIDDEN: { statusCode: 403, message: "Forbidden", success: false },
+    NOT_FOUND: { statusCode: 404, message: "Not Found", success: false },
+    CONFLICT: { statusCode: 409, message: "Conflict", success: false },
+    UNPROCESSABLE_ENTITY: { statusCode: 422, message: "Unprocessable Entity", success: false },
 
     // Server error status codes
-    INTERNAL_SERVER_ERROR: { statusCode: 500, message: "Internal Server Error" },
-    BAD_GATEWAY: { statusCode: 502, message: "Bad Gateway" },
-    SERVICE_UNAVAILABLE: { statusCode: 503, message: "Service Unavailable" },
-    GATEWAY_TIMEOUT: { statusCode: 504, message: "Gateway Timeout" },
-};
+    INTERNAL_SERVER_ERROR: { statusCode: 500, message: "Internal Server Error",success: false },
+    BAD_GATEWAY: { statusCode: 502, message: "Bad Gateway",success: false },
+    SERVICE_UNAVAILABLE: { statusCode: 503, message: "Service Unavailable",success: false },
+    GATEWAY_TIMEOUT: { statusCode: 504, message: "Gateway Timeout",success: false },
+} as const;
 
--- web/server/src/utils/env.ts
+++ web/server/src/utils/env.ts
@@ -19,6 +19,8 @@ export const PORT = requireEnv<number>("PORT", 3333);
 export const HOST = requireEnv<string>("HOST", "0.0.0.0");
 export const URL = requireEnv<string>("URL", "http://localhost");
 
+/*** ENCRYPTION ***/
+export const JWT_SECRET = requireEnv<string>("JWT_SECRET", "JWT_SECRET");
 export const SALT_ROUNDS = requireEnv<number>("SALT_ROUNDS", 10);
 
 /*** DATABASE ***/
@@ -34,17 +36,14 @@ export const S_WA_PHONE_ID = requireEnv<string>(
     "S_WA_PHONE_ID",
     "S_WA_PHONE_ID",
 );
-
 export const S_WA_ACCOUNT_NUMBER = requireEnv<string>(
     "S_WA_ACCOUNT_NUMBER",
     "S_WA_ACCOUNT_NUMBER",
 );
-
 export const S_WA_JOB_NOTIFY = requireEnv<string>(
     "S_WA_JOB_NOTIFY",
     "S_WA_JOB_NOTIFY",
 );
-
 export const HASH_ROUNDS = requireEnv<number>(
     "HASH_ROUNDS",
     10,
