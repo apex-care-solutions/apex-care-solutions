adccfa9367896ae45d47bc7a0ee5c1b16dcfbaf0
andreburg <andreludovicusburger@gmail.com>
2024-11-05 22:09:53 +0200

    fix: Add export for hash lib

--- web/app/src/lib/crypt/index.ts
+++ web/app/src/lib/crypt/index.ts
@@ -1,4 +1,4 @@
-async function hashPassword(password: string) {
+export async function hashPassword(password: string) {
     const encoder = new TextEncoder();
     const data = encoder.encode(password);
     const hashBuffer = await crypto.subtle.digest('SHA-256', data);
